1882fde8654280f3f1bc81f6893db62d355db839
pedrohemmel <phenrique_h2016@hotmail.com>
2021-09-02 11:38:42 -0300

    Senha terminada

--- senha.dart
+++ senha.dart
@@ -1,22 +1,54 @@
-
+import 'dart:io';
 import 'dart:math';
 
 
 void main() {
   //String nomeUsuario = stdin.readLineSync()!;
 
-  String alfabeto =
-    '12@#%3456789@#%ABCDEFGH@#%JKLMNPQRSTUVW@#%XYZabcdefghijk@#%mnopqrstuvwxyz@#%';
+  print('Vamos fazer sua senha forte!');
+
+  print('Digite quantos numeros você quer na senha: ');
+  int qtdNumeros = int.parse(stdin.readLineSync()!);
+
+  print('Digite quantas letras maiusculas você quer na senha: ');
+  int qtdAlfabetoMai = int.parse(stdin.readLineSync()!);
+
+  print('Digite quantas letras minusculas você quer na senha: ');
+  int qtdAlfabetoMin = int.parse(stdin.readLineSync()!);
+
+  print('Digite quantos carateres especiais você quer na senha: ');
+  int qtdCarater = int.parse(stdin.readLineSync()!);
+
+  String alfabetoMai =
+    'ABCDEFGHJKLMNPQRSTUVWXYZ';
+  String alfabetoMin =
+    'abcdefghijkmnopqrstuvwxyz';
+  String numeros =
+    '0123456789';
+  String caracter =
+    '!@#%&*()/?°';
+
   String senhaForte = '';
   print('');
   print('');
-  for(int i = 0; i < 9; i++) {
-    senhaForte = senhaForte + alfabeto[numerosRandomicos(alfabeto.length)];
-  }
-  print(senhaForte);
-  print('');
-  print('');
   
+  for(int i = 0; i < qtdNumeros; i++) {
+    senhaForte = senhaForte + numeros[numerosRandomicos(numeros.length)];
+  }
+  for(int i = 0; i < qtdAlfabetoMai; i++) {
+    senhaForte = senhaForte + alfabetoMai[numerosRandomicos(alfabetoMai.length)];
+  }
+  for(int i = 0; i < qtdAlfabetoMin; i++) {
+    senhaForte = senhaForte + alfabetoMin[numerosRandomicos(alfabetoMin.length)];
+  }
+  for(int i = 0; i < qtdCarater; i++) {
+    senhaForte = senhaForte + caracter[numerosRandomicos(caracter.length)];
+  }
+
+  print(senhaForte);
+
+  print('');
+  print('');
 }
 
 int numerosRandomicos(int valor) {
@@ -24,3 +56,4 @@ int numerosRandomicos(int valor) {
 
   return random.nextInt(valor);
 }
+
